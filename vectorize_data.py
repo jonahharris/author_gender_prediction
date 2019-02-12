76d0a8c86158aaff35e1ad11f55a04d56effe861
Jonah H. Harris <jonah.harris@gmail.com>
2019-02-12 13:10:00 -0500

    Updated tokenization in terms of stopwords

--- vectorize_data.py
+++ vectorize_data.py
@@ -8,6 +8,9 @@ from __future__ import print_function
 
 import pickle
 
+from textblob import Word
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Vectorization parameters
@@ -28,6 +31,39 @@ MIN_DOCUMENT_FREQUENCY = 2
 # Limit on the length of text sequences. Sequences longer than this will be truncated.
 MAX_SEQUENCE_LENGTH = 500
 
+# Customize stopwords
+stop_words = set(stopwords.words('english'))
+stop_words.remove('he')
+stop_words.remove('she')
+stop_words.remove('his')
+stop_words.remove('her')
+stop_words.remove('him')
+stop_words.remove('himself')
+stop_words.remove('herself')
+stop_words.remove('i')
+stop_words.remove('you')
+stop_words.remove('your')
+stop_words.remove('we')
+stop_words.remove('my')
+stop_words.remove('me')
+stop_words.remove('on')
+stop_words.remove('off')
+stop_words.remove('have')
+stop_words.remove('do')
+stop_words.remove('don')
+stop_words.remove('myself')
+stop_words.remove('being')
+stop_words.remove('to')
+stop_words.add('u')
+stop_words.add("'")
+stop_words.add(u"’")
+
+def custom_tokenizer(sentence):
+    word_tokens = word_tokenize(sentence) 
+    filtered_sentence = [w for w in word_tokens if not w in stop_words]
+    #words = [Word(word).lemmatize() for word in filtered_sentence]
+    words = filtered_sentence
+    return words
 
 def ngram_vectorize(train_texts, val_texts):
     """Vectorizes texts as ngram vectors.
@@ -46,8 +82,11 @@ def ngram_vectorize(train_texts, val_texts):
             'decode_error': 'replace',
             'analyzer': TOKEN_MODE,  # Split text into word tokens.
             'min_df': MIN_DOCUMENT_FREQUENCY,
-            'stop_words': 'english',
-            'max_features': TOP_K
+            'stop_words': None,
+            'max_features': TOP_K,
+            'tokenizer': custom_tokenizer,
+            'use_idf': True,
+            'max_df': 0.95
     }
     vectorizer = TfidfVectorizer(**kwargs)
 
